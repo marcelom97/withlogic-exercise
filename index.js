ceeb8755dcc4504613b32b3e889398ebc5513563
marcelom97 <marcelomollaj@gmail.com>
2021-02-10 14:58:12 +0200

    refactor: added json and public folder

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const { json } = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
+
+const asyncHandler = require('./utils/asyncHandler');
+const sort = require('./utils/quicksort');
 
 const app = express();
 
+// Serve static files
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// Mount logger
 app.use(morgan('dev'));
+
+app.use(json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
