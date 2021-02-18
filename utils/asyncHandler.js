463d07719c3ccad884ca7029958069031e2afddc
marcelom97 <marcelomollaj@gmail.com>
2021-02-18 16:59:15 +0200

    fix(impl #1): fixed the changes requested

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -4,8 +4,4 @@ const asyncHandler = (fn) => (...args) => {
   return Promise.resolve(fnReturn).catch(next);
 };
 
-// const asyncHandler = fn => (req, res, next) => {
-//   Promise.resolve(fn(req, res, next)).catch(next);
-// };
-
 module.exports = asyncHandler;
