f50c902fc9e75dfc884d83e6556c55ff9b100a95
marcelom97 <marcelomollaj@gmail.com>
2021-03-23 17:14:34 +0200

    refactor: renamed ordered_* to sorted_* in sort/tests.py

--- sort/tests.py
+++ sort/tests.py
@@ -3,17 +3,17 @@ from rest_framework.test import APITestCase, APIClient
 
 from .utils import sort
 
-arr = ['banana', 'apple', 'orange']
-ordered_arr = ['apple', 'banana', 'orange']
+arr = ["banana", "apple", "orange"]
+sorted_arr = ["apple", "banana", "orange"]
 
-arr1 = ['banana', 'apple', 'orange', 'apps']
-ordered_arr1 = ['apple', 'apps', 'banana', 'orange']
+arr1 = ["banana", "apple", "orange", "apps"]
+sorted_arr1 = ["apple", "apps", "banana", "orange"]
 
 
 class InsertionSortTests(TestCase):
     def test_array(self):
-        self.assertEqual(sort(arr), ordered_arr)
-        self.assertEqual(sort(arr1), ordered_arr1)
+        self.assertEqual(sort(arr), sorted_arr)
+        self.assertEqual(sort(arr1), sorted_arr1)
 
 
 class SortEndpointTests(APITestCase):
@@ -21,16 +21,14 @@ class SortEndpointTests(APITestCase):
         self.client = APIClient()
 
     def test_input_not_array(self):
-        test_dict = {
-            'data': arr
-        }
+        test_dict = {"data": arr}
 
-        request = self.client.post('/sort/', test_dict)
+        request = self.client.post("/sort/", test_dict)
 
         self.assertEqual(request.status_code, 400)
 
     def test_array_input(self):
-        response = self.client.post('/sort/', arr, format='json')
+        response = self.client.post("/sort/", arr, format="json")
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), ordered_arr)
+        self.assertEqual(response.json(), sorted_arr)
