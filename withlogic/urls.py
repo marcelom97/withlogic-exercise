ecb99aa2e977ee0f2d5822ee74ad01e11e279509
marcelom97 <marcelomollaj@gmail.com>
2021-03-23 17:38:47 +0200

    refactor: removed admin url

--- withlogic/urls.py
+++ withlogic/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
-from django.contrib import admin
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('sort.urls')),
+    path("", include("sort.urls")),
 ]
